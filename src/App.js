752b282ee1a16a812556a821e891d1daa3c2f712
SLeBron <sophieclebron@gmail.com>
2023-10-28 10:24:50 -0600

    updated app.js and app.css and added aboutMe page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/footer/footer.js';
+import NavBar from './components/navBar/navBar.js';
+import AboutMe from './pages/aboutMe/aboutMe.js';
+import Contact from './pages/contact/contact.js';
+import Homepage from './pages/homepage/homepage.js';
+import Projects from './pages/projects/projects.js';
+import Resume from './pages/resume/resume.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+        <NavBar />
+        <Routes>
+            <Route path='/aboutme' element={<AboutMe/>}/>
+            <Route path='/react-portfolio/' element={<Homepage/>}/>
+            <Route path='/projects' element={<Projects/>}/>
+            <Route path='/resume' element={<Resume/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+        <Footer />
+    </Router>
     </div>
   );
 }
